832451d8eefb91afafd60332cbf0c5a116fd4e67
Mahathi-s154 <1bi22ri028@bit-bangalore.edu.in>
2026-02-27 22:05:48 +0530

    enumeration choice wording in constraints

--- crates/wdl-analysis/src/stdlib/constraints.rs
+++ crates/wdl-analysis/src/stdlib/constraints.rs
@@ -185,7 +185,7 @@ impl Constraint for PrimitiveTypeConstraint {
     }
 }
 
-/// Represents a constraint that ensures the type is any enumeration variant.
+/// Represents a constraint that ensures the type is any enumeration choice.
 #[derive(Debug, Copy, Clone)]
 pub struct EnumVariantConstraint;
 
